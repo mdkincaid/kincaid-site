4251ffb48a9cf23b2c6475a14f524d86dc602bcf
Matthew Kincaid <matthew@kincaid.dev>
2022-07-13 20:16:09 -0400

    add environment check to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog'
 
 import { HelloDialogComponent } from './hello-dialog/hello-dialog.component';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public isDevEnv: boolean = true;
 
   constructor( public dialog: MatDialog ) {}
+
+  ngOnInit(): void {
+    if (environment.production) {
+      this.isDevEnv = false;
+    }
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(HelloDialogComponent, {

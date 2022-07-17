916e1598020e1f24fdf78e64fc66fdc73d862b2d
Matthew Kincaid <matthew@kincaid.dev>
2022-07-17 19:09:39 -0400

    add font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 
+import {
+	FaIconLibrary,
+	FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HelloDialogComponent } from './hello-dialog/hello-dialog.component';
@@ -16,9 +22,14 @@ import { MaterialModule } from './shared/material.module';
 		BrowserAnimationsModule,
 		BrowserModule,
 		FormsModule,
+		FontAwesomeModule,
 		MaterialModule,
 	],
 	providers: [{ provide: MatDialogRef, useValue: {} }, Title],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(library: FaIconLibrary) {
+		library.addIcons(faLinkedin, faGithub);
+	}
+}

230e15e44c8d9149d550e11fe3992071aab5bf2b
Matthew Kincaid <matthew@kincaid.dev>
2022-07-17 19:10:11 -0400

    add buttons for external links linkedin and github

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,7 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -10,6 +11,7 @@ const materialModules = [
 	MatButtonModule,
 	MatDialogModule,
 	MatIconModule,
+	MatListModule,
 	MatSlideToggleModule,
 	MatToolbarModule,
 	MatTooltipModule,

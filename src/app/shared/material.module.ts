4d22bd82f5f9d8d2adf63819c738e3f0ea6084c2
Matthew Kincaid <matthew@kincaid.dev>
2024-04-22 10:47:45 -0400

    feat: add Projects heading to project card section

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,7 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -10,13 +11,14 @@ import { NgModule } from '@angular/core';
 
 const materialModules = [
 	MatButtonModule,
+	MatCardModule,
 	MatDialogModule,
+	MatDividerModule,
 	MatIconModule,
 	MatListModule,
 	MatSlideToggleModule,
 	MatToolbarModule,
 	MatTooltipModule,
-	MatCardModule,
 ];
 
 @NgModule({
